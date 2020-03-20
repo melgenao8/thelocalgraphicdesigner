67f5e439a85183a9780f498817d93578d56800dd
Jarrell Houston <jhouston2882@yahoo.com>
2020-03-20 14:24:13 -0500

    updated menu slide out and fixed spacings

--- js/script.js
+++ js/script.js
@@ -13,3 +13,25 @@
 //         $(".nav-banner .nav-brand").hide();
 //     }
 // })
+
+$(document).ready(function () {
+
+    //sets horizontal scroll to center on core values card
+    // if ($(window).width() < 1331) {
+    //     var outerContent = $(".core-values");
+    //     var innerContent = $(".core-cards");
+    //     console.log(outerContent)
+    //     console.log(innerContent)
+    //     console.log(outerContent.width())
+    //     console.log(innerContent.width())
+    //     console.log((innerContent.width() - outerContent.width()) / 2)
+    //     outerContent.scrollLeft((innerContent.width() - outerContent.width()) / 2);
+
+    // }
+})
+
+// mobile menu slide from the left
+$('[data-toggle="slide-collapse"]').on('click', function () {
+    var navMenuCont = $($(this).data('target'));
+    navMenuCont.animate({ 'width': 'toggle', 'opacity': 'toggle' }, 350);
+});
