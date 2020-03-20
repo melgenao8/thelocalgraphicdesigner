44dc0d08fd741969096b7d0445f5a0c540b83ed3
Jarrell Houston <jhouston2882@yahoo.com>
2020-03-20 12:44:01 -0500

    responsive changes

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// $(window).on("resize", function (e) {
+//     console.log(e);
+//     if (e.target.innerWidth >= 992) {
+//         $(".nav-banner .nav-brand").hide();
+//     } else {
+//         $(".nav-banner .nav-brand").show();
+//     }
+// })
+
+// $(document).ready(function (e) {
+//     console.log($(window).width());
+//     if ($(window).width() >= 992) {
+//         $(".nav-banner .nav-brand").hide();
+//     }
+// })
