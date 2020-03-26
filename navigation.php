98f7799fea70dcfda91b710a095037bec8f491c3
Jarrell Houston <jhouston2882@yahoo.com>
2020-03-25 19:49:05 -0500

    added fonts and converted to php

---
+++ navigation.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg">
+    <a class="nav-brand" href="/"><img src="img/main-logo-TLGC.png" /></a>
+    <button class="navbar-toggler" type="button" data-toggle="slide-collapse" data-target="#menu" aria-controls="menu"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+    </button>
+
+    <!-- ==== NAVIGATION ==== -->
+    <div class="collapse navbar-collapse" id="menu">
+        <ul class="navbar-nav menu-nav">
+            <li class="nav-item">
+                <a class="nav-link sofia-sb" href="work.html">Work</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link sofia-sb" href="services.php">Services</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link sofia-sb" href="process.html">The Process</a>
+            </li>
+            <li class="nav-item nav-logo">
+                <a class="nav-link" href="index.php"><img src="img/main-logo-TLGC.png" /></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link sofia-sb" href="learn.html">Learn</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link sofia-sb" href="about.html">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link sofia-sb" href="connect.html">Connect</a>
+            </li>
+        </ul>
+        </ul>
+    </div>
+</nav>
