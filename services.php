60e797cf0b407e37068432ee56b029c147e45d60
Melissa <melgenao8@gmail.com>
2020-03-31 14:41:45 -0500

    Updating changes. Fixed sizing, animations, mobile responsiveness

--- services.php
+++ services.php
@@ -3,27 +3,46 @@
 <div class="container-fluid nav-banner">
     <?php require_once("navigation.php") ?>
 
+<!-- * ----------------------------------------
+ * opening title
+ * ---------------------------------------- -->
+
     <div class="opening">
         <div class="opening-title">
+            <div class="bounce-in-top">
             <!-- <h1 class="bebas-neue loose">DESIGN</h1><h1 class="sofia-sb-i">SERVICES</h1> -->
-            <h1 class="bebas-neue">DESIGN <span class="sofia-sb-i">Services</span></h1>
+                <h1 class="bebas-neue">DESIGN <span class="sofia-sb-i">Services</span></h1>
             <!-- <h1 class="sofia-sb">Design Services</h1> -->
             <!-- <p class="freight-i">great for bloggers, entrepreneurs, restaurants,
                 & the self-employed</p> -->
+            </div>
         </div>
         <div class="opening-text">
+        <div class="bounce-in-top-slower">
             <p class="sofia-light">Investing in something unfamiliar is not a good feeling.
                 Get acquianted with the branding process so that you
                 feel confident in your decision to create an effective
                 impression on your audience.</p>
+</div>
         </div>
     </div>
 </div>
 
+
+ <!-- * ----------------------------------------
+ * packages
+ * ---------------------------------------- -->
+
 <div class="packages">
-    <p class="sofia-sb">Popular Packages</p>
+
+    <h2 class="sofia-sb">Popular Packages</h3>
+    <p class="plantin-i">These packages are a great place to start.
+        Services are tailored specifically to each business.
+    </h2>
 
 
+
+  
 
     <div class="cards">
         <!-- BRANDING BASICS -- PACKAGE -->
@@ -138,6 +157,11 @@
     </div>
 </div>
 
+ <!-- * ----------------------------------------
+ * instant download
+ * ---------------------------------------- -->
+
+
 <div class="design-guide">
     <div class="download-form">
         <h2 class="plantin-i">instant download</h2>
@@ -152,9 +176,13 @@
     </div>
 </div>
 
+ <!-- * ----------------------------------------
+ * more branded content
+ * ---------------------------------------- -->
+
 <div class="two-text-col branded-content">
     <div class="title-col">
-        <h2 class="plantin-i">more</h2>
+        <h2 class="plantin-i">more </h2>
         <h3 class="sofia-sb">Branded <br> Content</h3>
     </div>
 
@@ -164,7 +192,7 @@
             <li>Package <br> Design</li>
             <li>Stationary <br> Set</li>
             <li>Poster <br> Design</li>
-            <li>Brochure</li>
+            <li>Brochures</li>
             <li>Web <br> Banners</li>
             <li>Website <br> Imagery</li>
             <li>Photography</li>
@@ -181,8 +209,10 @@
 
 
 </div>
-
-<div class="website-copy">
+ <!-- * ----------------------------------------
+ * common services
+ * ---------------------------------------- -->
+<!-- <div class="website-copy">
     <h3 class="sofia-sb">Need to Figure Out a Headline</h3>
     <h2 class="plantin-i">write about how important these two things are to be
         seen as a professional/expert in your indsutry.</h2>
@@ -190,7 +220,7 @@
     <div class="cards">
         <div class="card">
             <h4 class="plantin-i">the</h4>
-            <h3 class="sofia-sb">Website</h3>
+            <h3 class="sofia-sb">Strategy</h3>
 
             <p class="sofia">Write about the importance of a
                 good website. It’s not only about the
@@ -203,7 +233,7 @@
 
         <div class="card">
             <h4 class="plantin-i">the</h4>
-            <h3 class="sofia-sb">Copywriting</h3>
+            <h3 class="sofia-sb">Identity</h3>
 
             <p class="sofia">words are important. especially
                 if you’re trying to convert
@@ -214,8 +244,27 @@
                 or website copy, this is
                 important.</p>
         </div>
+
+        <div class="card">
+            <h4 class="plantin-i">the</h4>
+            <h3 class="sofia-sb">Application</h3>
+
+            <p class="sofia">words are important. especially
+                if you’re trying to convert
+                strangers into loyal customers.
+                it’s all about what you say, and
+                how you say it. wheather it’s
+                updating your mission statement,
+                or website copy, this is
+                important.</p>
+        </div>
+
     </div>
-</div>
+</div> -->
+
+ <!-- * ----------------------------------------
+ * monthly retainers
+ * ---------------------------------------- -->
 
 <div class="retainers">
     <h3 class="sofia-sb">Monthly Retainers</h3>
@@ -223,32 +272,47 @@
         entrprenuers who need fresh content every month, or
         need help keeping up with the brand execution</h2>
 
-    <div class="retainer-cols">
-        <div class="retainer-col">
-            <div class="icon">
-                <i class="far fa-images"></i>
+    
+        <div class="retainer-cols">
+        
+    <!-- ICON 1 -->
+    <div class="retainer-col">
+            <div class="icon-img-holder">
+                 <img src="img/icons/social-media-content-icon.svg">
             </div>
-            <p>Social Media <br> Content Creation</p>
+            <p>Social Media<br>Content Creation</p>
         </div>
-        <div class="retainer-col">
-            <div class="icon">
-                <i class="far fa-images"></i>
+    
+    <!-- ICON 2 -->
+    <div class="retainer-col">
+            <div class="icon-img-holder">
+                 <img src="img/icons/website-maintenance-icon.svg">
             </div>
-            <p>Website <br> Maintenance</p>
+            <p>Website<br>Maintenance</p>
         </div>
-        <div class="retainer-col">
-            <div class="icon">
-                <i class="far fa-images"></i>
+
+    <!-- ICON 3 -->
+    <div class="retainer-col">
+            <div class="icon-img-holder">
+                 <img src="img/icons/blog-writer-icon.svg">
             </div>
-            <p>Blog <br> Writing</p>
+            <p>Blog/Article<br>Writting</p>
         </div>
+
+    <!-- ICON 3 -->
         <div class="retainer-col">
-            <div class="icon">
-                <i class="far fa-images"></i>
+            <div class="icon-img-holder">
+                 <img src="img/icons/social-media-photographer-videographer.svg">
             </div>
-            <p>Videography <br> And Photography</p>
+            <p>Videography<br>And Photography</p>
         </div>
     </div>
+
+    
+     <!-- * ----------------------------------------
+ * pricing guide download
+ * ---------------------------------------- -->
+
 
     <div class="retainer-form">
         <div class="download-form">
@@ -263,6 +327,11 @@
         </div>
     </div>
 </div>
+
+
+ <!-- * ----------------------------------------
+ * work together button
+ * ---------------------------------------- -->
 
 <div class="two-col services-two-col">
     <div class="two-col-box">
